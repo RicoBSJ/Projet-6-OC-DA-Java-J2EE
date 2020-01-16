b1de69851233ff9898562d43ae5e6cb2166d07e4
ricobsj <ricobsj@localhost>
2020-01-17 00:27:53 +0100

    First Repo

--- escalade-webapp/src/main/java/com/octest/servlets/Accueil.java
+++ escalade-webapp/src/main/java/com/octest/servlets/Accueil.java
@@ -1,32 +1,28 @@
 package com.octest.servlets;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Test")
-public class Test extends HttpServlet {
+@WebServlet("/Accueil")
+public class Accueil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	public Accueil() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		Site site = new Site();
-		site.setNomSite("Le site des têtes brulées !!!");
-		site.setDescriptSite("Des voies de ouf !!!");
-		site.setCotationSite("Des voies pour experts et novices");
-
-		request.setAttribute("site", site);
-
-		this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
+		this.getServletContext().getRequestDispatcher("/WEB-INF/accueil.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 }
---
+++ escalade-webapp/src/main/webapp/WEB-INF/bonjour.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Escalade-Webapp</title>
+</head>
+<body>
+	<p>
+		<c:out value="Bonjour !" />
+	</p>
+</body>
+</html>
