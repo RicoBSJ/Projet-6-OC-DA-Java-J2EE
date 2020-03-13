b36d42d01d48ea679764df8d5377b337cb88c5f3
ricobsj <ricobsj@localhost>
2020-03-14 00:35:19 +0100

    First Repo

--- escalade-consumer/src/main/java/com/aubrun/eric/projet6/consumer/DAO/SiteDAO.java
+++ escalade-consumer/src/main/java/com/aubrun/eric/projet6/consumer/DAO/SiteDAO.java
@@ -11,70 +11,36 @@ import com.aubrun.eric.projet6.model.bean.Site;
 
 public class SiteDAO {
 
-	SessionFactory factory = HibernateUtils.getSessionFactory();
+    SessionFactory factory = HibernateUtils.getSessionFactory();
 
-	public List<Site> recupererSites() {
+    public List<Site> recupererSites() {
 
-		Session session = factory.getCurrentSession();
-		List<Site> sites = null;
+        Session session = factory.getCurrentSession();
+        List<Site> sites = null;
 
-		try {
-			session.getTransaction().begin();
-			String q = "SELECT s FROM Site s";
-			Query<Site> query = session.createQuery(q);
-			sites = query.getResultList();
-			session.getTransaction().commit();
+        try {
+            session.getTransaction().begin();
+            String q = "SELECT s FROM Site s";
+            Query<Site> query = session.createQuery( q );
+            sites = query.getResultList();
+            session.getTransaction().commit();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			// Rollback in case of an error occurred.
-			session.getTransaction().rollback();
-		}
-		return sites;
-	}
+        } catch ( Exception e ) {
+            e.printStackTrace();
+            // Rollback in case of an error occurred.
+            session.getTransaction().rollback();
+        }
+        return sites;
+    }
 
-	public List<Site> ajouterSite() {
+    public List<Site> ajouterSite() {
 
-		return null;
-	}
+        return null;
+    }
 
-	public List<Site> afficherDetails() {
+    public List<Site> afficherDetails() {
 
-		Session session = factory.getCurrentSession();
-		List<Site> sites = null;
+        return null;
 
-		try {
-			session.getTransaction().begin();
-			String q = "SELECT s FROM Site s";
-
-			String image = "${photo.chemin}";
-
-			switch (image) {
-			case "images/ablon_marc_daviet_crop_995x516.jpg":
-				<c:url value="${ site.nomSite }" />;
-				break;
-			case "images/ailefroide_escalade_5__crop_995x516.jpg":
-				System.out.println("Site Aile froide");
-				break;
-			case "images/annot-fred-oddo-dans-6c__crop_995x516.jpg":
-				System.out.println("Site Annot");
-				break;
-			case "images/antalya1_dr_crop_995x516.jpg":
-				System.out.println("Site Antaya");
-				break;
-			case "images/arudy_falaise__crop_995x516.jpg":
-				System.out.println("Site Arudy");
-				break;
-			default:
-				System.out.println("Autre site");
-			}
-
-			return null;
-		} catch (Exception e) {
-			e.printStackTrace();
-			// Rollback in case of an error occurred.
-			session.getTransaction().rollback();
-		}
-		return sites;
-	}
+    }
 }
