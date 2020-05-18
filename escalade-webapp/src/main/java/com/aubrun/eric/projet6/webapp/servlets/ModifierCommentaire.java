a9aaf6c04780a0d0bee2339bf9eb8d9654296f72
ricobsj <ricobsj@localhost>
2020-05-18 23:44:48 +0200

    AddServletFormJsp UpdateDeleteComment HadToContinue

--- escalade-webapp/src/main/java/com/aubrun/eric/projet6/webapp/servlets/ModifierCommentaire.java
+++ escalade-webapp/src/main/java/com/aubrun/eric/projet6/webapp/servlets/ModifierCommentaire.java
@@ -1,41 +1,78 @@
 package com.aubrun.eric.projet6.webapp.servlets;
 
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.aubrun.eric.projet6.business.service.CommentaireService;
+import com.aubrun.eric.projet6.model.bean.Commentaire;
+import com.aubrun.eric.projet6.model.bean.Utilisateur;
+import com.aubrun.eric.projet6.webapp.forms.ModifierCommentaireForm;
 
 /**
  * Servlet implementation class ModifierCommentaire
  */
-@WebServlet("/ModifierCommentaire")
+@WebServlet( "/ModifierCommentaire" )
 public class ModifierCommentaire extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ModifierCommentaire() {
-        super();
-        // TODO Auto-generated constructor stub
+
+    private static final long  serialVersionUID   = 1L;
+
+    public static final String ATT_COMMENTAIRE    = "commentaire";
+    public static final String ATT_FORM           = "form";
+    public static final String ATT_SESSION_USER   = "sessionUtilisateur";
+    public static final String VUE                = "/WEB-INF/jsp/supprimerCommentaire.jsp";
+
+    private CommentaireService commentaireService = new CommentaireService();
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* Affichage de la page d'inscription */
+
+        HttpSession session = request.getSession();
+
+        Utilisateur utilisateur = (Utilisateur) session.getAttribute( ATT_SESSION_USER );
+
+        if ( utilisateur == null ) {
+            response.setStatus( HttpServletResponse.SC_FORBIDDEN );
+            throw new RuntimeException();
+        }
+
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    public void doPost( HttpServletRequest request, HttpServletResponse response )
+            throws ServletException, IOException {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+        /* Préparation de l'objet formulaire */
+        ModifierCommentaireForm form = new ModifierCommentaireForm();
 
+        /* Récupération de la session depuis la requête */
+        HttpSession session = request.getSession();
+
+        Utilisateur connectedUser = (Utilisateur) session.getAttribute( ATT_SESSION_USER );
+
+        if ( connectedUser == null ) {
+            response.setStatus( HttpServletResponse.SC_FORBIDDEN );
+            throw new RuntimeException();
+        } else {
+            Commentaire commentaire = form.modifierCommentaire( request );
+
+            commentaire.setUtilisateur( connectedUser );
+            commentaire.setDate( new Date() );
+
+            commentaireService.updateCommentaire( commentaire );
+
+            session.setAttribute( ATT_SESSION_USER, connectedUser );
+
+            request.setAttribute( ATT_FORM, form );
+            request.setAttribute( ATT_COMMENTAIRE, commentaire );
+
+            this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+        }
+    }
 }
--- escalade-webapp/src/main/java/com/aubrun/eric/projet6/webapp/servlets/SupprimerCommentaire.java
+++ escalade-webapp/src/main/java/com/aubrun/eric/projet6/webapp/servlets/SupprimerCommentaire.java
@@ -1,32 +1,76 @@
 package com.aubrun.eric.projet6.webapp.servlets;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.aubrun.eric.projet6.business.service.CommentaireService;
+import com.aubrun.eric.projet6.model.bean.Commentaire;
+import com.aubrun.eric.projet6.model.bean.Utilisateur;
+import com.aubrun.eric.projet6.webapp.forms.SupprimerCommentaireForm;
 
 @WebServlet( "/SupprimerCommentaire" )
 public class SupprimerCommentaire extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+    private static final long  serialVersionUID   = 1L;
 
-    protected void doGet( HttpServletRequest request, HttpServletResponse response )
-            throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append( "Served at: " ).append( request.getContextPath() );
+    public static final String ATT_COMMENTAIRE    = "commentaire";
+    public static final String ATT_FORM           = "form";
+    public static final String ATT_SESSION_USER   = "sessionUtilisateur";
+    public static final String VUE                = "/WEB-INF/jsp/supprimerCommentaire.jsp";
+
+    private CommentaireService commentaireService = new CommentaireService();
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* Affichage de la page d'inscription */
+
+        HttpSession session = request.getSession();
+
+        Utilisateur utilisateur = (Utilisateur) session.getAttribute( ATT_SESSION_USER );
+
+        if ( utilisateur == null ) {
+            response.setStatus( HttpServletResponse.SC_FORBIDDEN );
+            throw new RuntimeException();
+        }
+
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost( HttpServletRequest request, HttpServletResponse response )
+    public void doPost( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
 
+        /* Préparation de l'objet formulaire */
+        SupprimerCommentaireForm form = new SupprimerCommentaireForm();
+
+        /* Récupération de la session depuis la requête */
+        HttpSession session = request.getSession();
+
+        Utilisateur connectedUser = (Utilisateur) session.getAttribute( ATT_SESSION_USER );
+
+        if ( connectedUser == null ) {
+            response.setStatus( HttpServletResponse.SC_FORBIDDEN );
+            throw new RuntimeException();
+        } else {
+            Commentaire commentaire = form.supprimerCommentaire( request );
+
+            commentaire.setUtilisateur( connectedUser );
+            commentaire.setDate( new Date() );
+
+            // commentaireService.deleteCommentaire( Commentaire idCommentaire
+            // );
+
+            session.setAttribute( ATT_SESSION_USER, connectedUser );
+
+            request.setAttribute( ATT_FORM, form );
+            request.setAttribute( ATT_COMMENTAIRE, commentaire );
+
+            this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+        }
     }
-
 }
