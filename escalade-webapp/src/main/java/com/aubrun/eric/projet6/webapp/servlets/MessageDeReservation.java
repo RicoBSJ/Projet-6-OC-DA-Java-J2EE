0ea97279e7af5a92fb02afb98148c20bcd71c918
ricobsj <ricobsj@localhost>
2020-07-10 01:46:50 +0200

    CorrectionServlet

--- escalade-webapp/src/main/java/com/aubrun/eric/projet6/webapp/servlets/MessageDeReservation.java
+++ escalade-webapp/src/main/java/com/aubrun/eric/projet6/webapp/servlets/MessageDeReservation.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpSession;
 
 import com.aubrun.eric.projet6.business.service.MessageService;
 import com.aubrun.eric.projet6.business.service.TopoService;
-import com.aubrun.eric.projet6.business.service.UtilisateurService;
 import com.aubrun.eric.projet6.model.bean.Message;
 import com.aubrun.eric.projet6.model.bean.Topo;
 import com.aubrun.eric.projet6.model.bean.Utilisateur;
@@ -19,16 +18,15 @@ import com.aubrun.eric.projet6.model.bean.Utilisateur;
 @WebServlet( "/messageDeReservation" )
 public class MessageDeReservation extends HttpServlet {
 
-    private static final long  serialVersionUID   = 1L;
+    private static final long  serialVersionUID = 1L;
 
-    public static final String ATT_MESSAGE        = "message";
-    public static final String ATT_FORM           = "form";
-    public static final String ATT_SESSION_USER   = "sessionUtilisateur";
-    public static final String VUE                = "/WEB-INF/jsp/messageDeReservation.jsp";
+    public static final String ATT_MESSAGE      = "message";
+    public static final String ATT_FORM         = "form";
+    public static final String ATT_SESSION_USER = "sessionUtilisateur";
+    public static final String VUE              = "/WEB-INF/jsp/messageDeReservation.jsp";
 
-    private MessageService     messageService     = new MessageService();
-    private TopoService        topoService        = new TopoService();
-    private UtilisateurService utilisateurService = new UtilisateurService();
+    private MessageService     messageService   = new MessageService();
+    private TopoService        topoService      = new TopoService();
 
     protected void doGet( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException {
